401f12eba9cc7558c9666cc514b3d8423e2852cb
Marvour Young <marvour@gmail.com>
2010-09-27 20:14:13 +0800

    start ...

---
+++ README.rst
@@ -0,0 +1,10 @@
+======================
+Vealous
+======================
+
+:Author: marvour<marvour@gmail.com>
+:Blog: http://i.shiao.org
+
+INTRO
+=====================
+Introduction at http://i.shiao.org/a/pydouban
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'pydouban',
+    version = '1.0.0',
+    description = 'Lightweight Python Douban API Library',
+    author = 'Marvour',
+    author_email = 'marvour@gmail.com',
+    license = 'BSD License',
+    url = 'http://i.shiao.org/a/pydouban',
+    packages = ['pydouban'],
+)
